877012b2d65295f646536de3cd8612bcd3ec5882
NiuMa <2493378918@qq.com>
2022-12-08 10:02:22 +0800

    Reaf: init project

---
+++ src/main/java/online/niuma/xiaocubao/XiaocubaoApplication.java
@@ -0,0 +1,13 @@
+package online.niuma.xiaocubao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaocubaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaocubaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/niuma/xiaocubao/XiaocubaoApplicationTests.java
@@ -0,0 +1,13 @@
+package online.niuma.xiaocubao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaocubaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
